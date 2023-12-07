88368a0d9d62f698ea542d97e797fbec5a6492e4
Rik <rik@pqina.nl>
2023-12-07 09:20:25 -0400

    add local_modules notes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 
+// Uncomment if installing Pintura in ./local_modules (also requires installing @pqina/svelte-pintura to local_modules)
+// import { searchForWorkspaceRoot } from "vite";
+
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+
+  // Uncomment if installing Pintura in ./local_modules
+  // server: {
+  //   fs: {
+  //     allow: [searchForWorkspaceRoot(process.cwd()), "/local_modules"],
+  //   },
+  // },
 };
 
 export default config;
